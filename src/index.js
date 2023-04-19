b13c0e3afb3bc133f7932a1c7ba24ddb1de5bcaa
zbys1av <zbyslav13@gmail.com>
2023-04-19 04:39:46 +0300

    added Brands HTML/CSS/JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './style.scss';
-
 console.log('Webpack starter with SCSS and dev server');
+
+//? через focus зробити, щоб вони не пропадали/пропадали 
+
+// TOP WRITING SERVICES (--top-services)
+const topServices = document.querySelector('.header__item--dropdown-top');
+const topSections = document.querySelector('.sections--top');
+
+function toggleTopServices(){
+    topSections.classList.toggle('open');
+}
+
+topServices.addEventListener("mouseover", toggleTopServices);
+
+// BRANDS
+const brand = document.querySelector('.header__item--dropdown-brand');
+const brandSections = document.querySelector('.sections--brands');
+
+function toggleBrands(){
+    brandSections.classList.toggle('open');
+}
+
+brand.addEventListener("mouseover", toggleBrands);
